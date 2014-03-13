750f32ab2aacfbad634ae1fe6a54f31dff893320
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-03-13 09:44:12 -0500

    Added org mode and some RVM stuff.

--- bashrc
+++ bashrc
@@ -1,3 +1,5 @@
 source ~/dotfiles/bash/aliases
 source ~/dotfiles/bash/paths
 source ~/dotfiles/bash/config
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
--- emacs.d/aliases.el
+++ emacs.d/aliases.el
@@ -1,2 +1,7 @@
 ;; Alias y/n for yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
