5e2e19bd5045d91a324069166f9918e187baef9b
Sean Eshbaugh <seaneshbaugh@gmail.com>
2020-08-26 13:43:06 -0500

    Removed org-mode config from aliases.

--- emacs.d/aliases.el
+++ emacs.d/aliases.el
@@ -1,7 +1,2 @@
 ;; Alias y/n for yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
