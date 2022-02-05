108dc33a371bb3b90e9ab7eeeb06a10f34f2742e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2022-02-05 07:01:29 -0600

    Added clojure-mode, cobol-mode, git-gutter, handlebars-mode, and neotree. Configured backups. Changed some display settings.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,6 +22,59 @@
 		       :repo "omar-polo/a68-mode"
 		       :branch "main"))
 
+(use-package clojure-mode)
+
+(use-package cobol-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.cob$" . cobol-mode)))
+
+(use-package git-gutter
+  :init
+  (global-git-gutter-mode t))
+
+(use-package handlebars-mode
+  :custom
+  (handlebars-basic-offset 4 "The basic indentation offset for handlebars."))
+
+(use-package neotree
+  :init
+  (global-set-key [f8] 'neotree-toggle)
+  :custom
+  (neo-window-width 50 "Specifies the width of the NeoTree window.")
+  :custom-face
+  (neo-file-link-face ((t (:foreground "brightwhite")))))
+
 (use-package which-key
   :config
   (which-key-mode))
+
+;; Backups
+
+;; Don't clutter directories with backup files, put them all in .emacs.d directory.
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; Silently delete execess backup versions.
+(setq delete-old-versions t)
+
+;; Only keep the last 1000 backups of a file.
+(setq kept-old-versions 1000)
+
+;; Even version controlled files will be backed up.
+(setq vc-make-backup-files t)
+
+;; Use version numbers for backup files.
+(setq version-control t)
+
+;; Display
+
+;; Add a space between line numbers and the buffer.
+(setq display-line-numbers "%4%d ")
+
+;; Show line numbers.
+(global-display-line-numbers-mode t)
+
+;; Show column number.
+(setq column-number-mode t)
+
+;; Show trailing whitespace.
+(setq-default show-trailing-whitespace t)
