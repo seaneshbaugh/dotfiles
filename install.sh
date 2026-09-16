3b00d07c09f3216cf030e2d7ff37703e75ba2c81
Sean Eshbaugh <seaneshbaugh@gmail.com>
2026-09-15 20:17:41 -0500

    Added detect_platform, moved dotfile installation into a function.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ function show_help {
     echo "  -b[backup_directory]    set the backup directory"
     echo "  -d                      dry run, do not touch the file system (also sets verbose flag)"
     echo "  -f[files]               set the files to install"
-    echo "  -h                      show this message"
+    echo "  -h                      show this message and exit"
     echo "  -v                      show verbose output"
 }
 
@@ -26,114 +26,72 @@ function abspath {
     fi
 }
 
-INSTALL_SCRIPT_PATH="$(abspath "$0")"
-DOTFILE_DIRECTORY="$(dirname "$INSTALL_SCRIPT_PATH")"
-BACKUP_DIRECTORY="${DOTFILE_DIRECTORY}-backup"
-DEFAULT_DOTFILES=(
-    bash_profile
-    bashrc
-    emacs.d
-    gemrc
-    gitconfig
-    gnus.el
-    tool-versions
-    vimrc
-    zlogin
-    zlogout
-    zshenv
-    zshrc
-)
-DOTFILES=()
-DRYRUN=0
-VERBOSE=0
-
-while getopts "b:df:hv" opt; do
-    case "$opt" in
-        b)
-            BACKUP_DIRECTORY=$OPTARG
-
-            if [[ "$BACKUP_DIRECTORY" = "$HOME" || "$BACKUP_DIRECTORY" = "$DOTFILE_DIRECTORY" ]]; then
-                echo "Error: Cannot use home directory or $DOTFILE_DIRECTORY as backup directory." >&2
-                exit 1
+function detect_platform {
+    case "$(uname -s)" in
+        Darwin)
+            echo "darwin"
+            ;;
+        Linux)
+            if [[ -r /etc/os-release ]]; then
+                . /etc/os-release
+                case "$ID" in
+                    ubuntu)
+                        echo "ubuntu"
+                        ;;
+                    fedora)
+                        echo "fedora"
+                        ;;
+                    *)
+                        # Some other version of Linux without specific configuration.
+                        echo "linux"
+                        ;;
+                esac
+            else
+                echo "linux"
             fi
             ;;
-        d)
-            DRYRUN=1
-            VERBOSE=1
-            ;;
-        f)
-            DOTFILES+=("$OPTARG")
-            ;;
-        h)
-            show_help
-            exit 0
-            ;;
-        v)
-            VERBOSE=1
-            ;;
         *)
-            echo "Error: Invalid switch." >&2
-            show_help
-            exit 1
+            echo "unknown"
             ;;
     esac
-done
+}
 
-if [[ ${#DOTFILES[@]} -eq 0 ]]; then
-    DOTFILES=("${DEFAULT_DOTFILES[@]}")
-fi
-
-if [[ "$DRYRUN" -eq 1 ]]; then
-    echo "Doing dry run. No files will be modified."
-fi
-
-if [[ "$VERBOSE" -eq 1 ]]; then
-    echo "Installing dotfiles from $DOTFILE_DIRECTORY."
-fi
-
-if [[ "$VERBOSE" -eq 1 ]]; then
-    echo "Creating backup directory $BACKUP_DIRECTORY."
-fi
-
-if [[ "$DRYRUN" -eq 0 ]]; then
-    mkdir -p "$BACKUP_DIRECTORY"
-fi
-
-for dotfile in "${DOTFILES[@]}"; do
-    source_path="$DOTFILE_DIRECTORY/$dotfile"
-    dotfile_path="$HOME/.$dotfile"
+function install_dotfile {
+    local dotfile="$1"
+    local source_path="$DOTFILE_DIRECTORY/$dotfile"
+    local dotfile_path="$HOME/.$dotfile"
 
     if [[ ! -e "$source_path" ]]; then
         echo "Warning: $source_path does not exist, skipping." >&2
-        continue
+        return
     fi
 
-    if [[ -e $dotfile_path || -L $dotfile_path ]]; then
-        dotfile_name="$(basename "$dotfile_path")"
-        backup_path="$BACKUP_DIRECTORY/$dotfile_name"
+    if [[ -e "$dotfile_path" || -L "$dotfile_path" ]]; then
+        local dotfile_name="$(basename "$dotfile_path")"
+        local backup_path="$BACKUP_DIRECTORY/$dotfile_name"
 
         if [[ "$VERBOSE" -eq 1 ]]; then
             echo "$dotfile_path already exists."
         fi
 
-        if [[ -L $dotfile_path ]]; then
-            symlink_path="$(readlink "$dotfile_path")"
+        if [[ -L "$dotfile_path" ]]; then
+            local symlink_path="$(readlink "$dotfile_path")"
 
             if [[ "$VERBOSE" -eq 1 ]]; then
                 echo "$dotfile_path is a symlink pointing to $symlink_path."
             fi
 
-            if [[ -e $dotfile_path ]]; then
+            if [[ -e "$dotfile_path" ]]; then
                 if [[ "$VERBOSE" -eq 1 ]]; then
                     echo "$dotfile_path is a valid symlink."
                 fi
 
-                if [[ $dotfile_path -ef $source_path ]]; then
+                if [[ "$dotfile_path" -ef "$source_path" ]]; then
                     if [[ "$VERBOSE" -eq 1 ]]; then
                         echo "$dotfile_path is already pointing $source_path, skipping."
                     fi
 
-                    continue
+                    return
                 else
                     if [[ "$VERBOSE" -eq 1 ]]; then
                         echo "$dotfile_path is pointing to a file other than $source_path."
@@ -143,7 +101,7 @@ for dotfile in "${DOTFILES[@]}"; do
                         echo "Backing up old symlink with path correction."
                     fi
 
-                    new_symlink_path="$(realpath "$dotfile_path" "--relative-to=$BACKUP_DIRECTORY")"
+                    local new_symlink_path="$(realpath "$dotfile_path" "--relative-to=$BACKUP_DIRECTORY")"
 
                     if [[ "$DRYRUN" -eq 0 ]]; then
                         ln -s "$new_symlink_path" "$backup_path"
@@ -194,4 +152,81 @@ for dotfile in "${DOTFILES[@]}"; do
             ln -s "$source_path" "$dotfile_path"
         fi
     fi
+}
+
+INSTALL_SCRIPT_PATH="$(abspath "$0")"
+DOTFILE_DIRECTORY="$(dirname "$INSTALL_SCRIPT_PATH")"
+BACKUP_DIRECTORY="${DOTFILE_DIRECTORY}-backup"
+DEFAULT_DOTFILES=(
+    bash_profile
+    bashrc
+    emacs.d
+    gemrc
+    gitconfig
+    gnus.el
+    tool-versions
+    vimrc
+    zlogin
+    zlogout
+    zshenv
+    zshrc
+)
+DOTFILES=()
+DRYRUN=0
+VERBOSE=0
+
+while getopts "b:df:hv" opt; do
+    case "$opt" in
+        b)
+            BACKUP_DIRECTORY="$OPTARG"
+
+            if [[ "$BACKUP_DIRECTORY" = "$HOME" || "$BACKUP_DIRECTORY" = "$DOTFILE_DIRECTORY" ]]; then
+                echo "Error: Cannot use home directory or $DOTFILE_DIRECTORY as backup directory." >&2
+                exit 1
+            fi
+            ;;
+        d)
+            DRYRUN=1
+            VERBOSE=1
+            ;;
+        f)
+            DOTFILES+=("$OPTARG")
+            ;;
+        h)
+            show_help
+            exit 0
+            ;;
+        v)
+            VERBOSE=1
+            ;;
+        *)
+            echo "Error: Invalid switch." >&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+if [[ ${#DOTFILES[@]} -eq 0 ]]; then
+    DOTFILES=("${DEFAULT_DOTFILES[@]}")
+fi
+
+if [[ "$DRYRUN" -eq 1 ]]; then
+    echo "Doing dry run. No files will be modified."
+fi
+
+if [[ "$VERBOSE" -eq 1 ]]; then
+    echo "Installing dotfiles from $DOTFILE_DIRECTORY."
+fi
+
+if [[ "$VERBOSE" -eq 1 ]]; then
+    echo "Creating backup directory $BACKUP_DIRECTORY."
+fi
+
+if [[ "$DRYRUN" -eq 0 ]]; then
+    mkdir -p "$BACKUP_DIRECTORY"
+fi
+
+for dotfile in "${DOTFILES[@]}"; do
+    install_dotfile "$dotfile"
 done
